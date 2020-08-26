7ee8ab0f704a2716c1f35eb3bb4442db3a5d8186
Daniel Ryu <danielryu@MacBook-Pro.attlocal.net>
2020-08-25 18:33:12 -0700

    sleep test

--- signal_check_expect.sh
+++ signal_check_expect.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect
 
 #If it all goes pear shaped the script will timeout after 20 seconds.
-set timeout 60
+set timeout 20
 #First argument is assigned to the variable name
 set name [lindex $argv 0]
 #Second argument is assigned to the variable user
@@ -10,15 +10,18 @@ set user [lindex $argv 1]
 set password [lindex $argv 2]
 #This spawns the telnet program and connects it to the variable name
 spawn telnet $name 
+
 #The script expects login
 expect "UserDevice login:" 
+
 #The script sends the user variable
 send "$user "
-sleep 2
+
 #The script expects Password
 expect "Password:"
+
 #The script sends the password variable
 send "$password "
-sleep 2
+
 #This hands control of the keyboard over to you (Nice expect feature!)
 send "AT+MWRSSI"
